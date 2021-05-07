426dcee0f2d7445bfb2be3a1073d6d02b68ed91f
Ben-Weisman <54437197+Ben-Weisman@users.noreply.github.com>
2021-05-07 19:40:43 +0300

    Worked on Logic dll only.

---
+++ Logic/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+
+    public enum eBoardSigns
+    {
+        X = 'X',
+        O = 'O',
+        Blank = ' '
+    }
+
+}
--- Logic/Player.cs
+++ Logic/Player.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Commit test
 
 namespace Logic
 {
     class Player
     {
         private readonly char m_Sign;
-
+        private int m_score;
+        public int Score
+        {
+            get;
+            set;
+        }
         public Player(char i_DesiredSign)
         {
             this.m_Sign = i_DesiredSign;
+            Score = 0;
         }
     }
 }
